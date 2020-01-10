878cba46aecf9a1dbc2079e22f74497b0b48432f
manan2002 <31039184+manan2002@users.noreply.github.com>
2020-01-10 23:25:34 +0530

    Update diagonal-difference-hackerrank.py

--- diagonal-difference-hackerrank.py
+++ diagonal-difference-hackerrank.py
@@ -1,1 +1,18 @@
-
+def diagonalDifference(arr):
+    leftDiagonalSum, rightDiagonalSum = 0, 0
+    arr_len = len(arr)
+    i = 0
+    j = 0
+    while(i < arr_len):
+        leftDiagonalSum += arr[i][j]
+        i += 1
+        j += 1
+    
+    i = 0
+    j = arr_len - 1
+    while(i < arr_len):
+        rightDiagonalSum += arr[i][j]
+        i += 1
+        j -= 1
+    
+    return abs(leftDiagonalSum - rightDiagonalSum)
