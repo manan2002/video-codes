949d4af74bfc5975a2394c5f3297a7f751625513
manan2002 <31039184+manan2002@users.noreply.github.com>
2020-01-11 17:20:07 +0530

    Create divisible-sum-hackerrank.py

---
+++ divisible-sum-hackerrank.py
@@ -0,0 +1,17 @@
+#simple method
+def divisibleSumPairs(n, k, ar):
+
+    count = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if (ar[i] + ar[j]) % k == 0:
+                count += 1
+    return count
+    
+#list comprehension method
+def divisibleSumPairs(n, k, ar):
+    return len([(ar[i], ar[j]) 
+      for i in range(n) 
+      for j in range(i+1, n)
+      if (ar[i] + ar[j]) % k == 0
+      ])
